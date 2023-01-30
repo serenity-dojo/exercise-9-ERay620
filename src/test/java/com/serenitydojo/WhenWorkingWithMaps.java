73810e4a1e7d118c153d7fd14204f21e6091a017
Berk620 <kifmyvz@gmail.com>
2023-01-30 20:16:27 +0100

    exercise-9-ERay620 is completed

--- src/test/java/com/serenitydojo/WhenWorkingWithMaps.java
+++ src/test/java/com/serenitydojo/WhenWorkingWithMaps.java
@@ -3,6 +3,7 @@ package com.serenitydojo;
 import com.serenitydojo.model.FoodType;
 import org.junit.Test;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import static com.serenitydojo.model.FoodType.*;
@@ -28,6 +29,11 @@ public class WhenWorkingWithMaps {
         //  France -> Paris
         //  Germany -> Berlin
 
+        countryCapitals = new HashMap<>();
+        countryCapitals.put("UK", "London");
+        countryCapitals.put("France", "Paris");
+        countryCapitals.put("Germany", "Berlin");
+
         assertThat(countryCapitals.get("UK")).isEqualTo("London");
         assertThat(countryCapitals.get("France")).isEqualTo("Paris");
         assertThat(countryCapitals.get("Germany")).isEqualTo("Berlin");
@@ -42,6 +48,11 @@ public class WhenWorkingWithMaps {
         //  "dog" -> DELUXE_DOG_FOOD
         //  "hamster" -> LETTUCE
 
+        favoriteFood = new HashMap<>();
+        favoriteFood.put("cat", TUNA);
+        favoriteFood.put("dog", DELUXE_DOG_FOOD);
+        favoriteFood.put("hamster", LETTUCE);
+
         assertThat(favoriteFood.get("cat")).isEqualTo(TUNA);
         assertThat(favoriteFood.get("dog")).isEqualTo(DELUXE_DOG_FOOD);
         assertThat(favoriteFood.get("hamster")).isEqualTo(LETTUCE);
@@ -55,6 +66,9 @@ public class WhenWorkingWithMaps {
         //  "cat" -> TUNA
         //  "dog" -> DELUXE_DOG_FOOD
         //  "hamster" -> LETTUCE
+
+        favoriteFood =Map.of("cat", TUNA, "dog", DELUXE_DOG_FOOD, "hamster",LETTUCE);
+
 
         assertThat(favoriteFood.get("cat")).isEqualTo(TUNA);
         assertThat(favoriteFood.get("dog")).isEqualTo(DELUXE_DOG_FOOD);
@@ -72,6 +86,7 @@ public class WhenWorkingWithMaps {
 
         // TODO: Check that the map contains a key of "dog"
         boolean containsDog = false;
+        containsDog = favoriteFood.containsKey("dog");
 
         assertThat(containsDog).isTrue();
     }
@@ -86,6 +101,7 @@ public class WhenWorkingWithMaps {
 
         // TODO: Check that the map contains a value of TUNA
         boolean containsTuna = false;
+        containsTuna = favoriteFood.containsValue(TUNA);
 
         assertThat(containsTuna).isTrue();
     }
